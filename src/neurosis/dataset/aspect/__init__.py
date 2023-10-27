3179ed1306d290c47a2065376e34e671472474e5
Andrew Powers-Holmes <aholmes@omnom.net>
2023-10-27 14:33:32 +1100

    dataset: aspect: export lists from main module namespace

---
+++ src/neurosis/dataset/aspect/__init__.py
@@ -0,0 +1,9 @@
+from .bucket import AspectBucket, AspectBucketList
+from .lists import SDXLBucketList, WDXLBucketList
+
+__all__ = [
+    "AspectBucket",
+    "AspectBucketList",
+    "SDXLBucketList",
+    "WDXLBucketList",
+]
