978cf5b69f650370ee704ee13e36519cae31e873
Andrew Powers-Holmes <aholmes@omnom.net>
2023-11-16 21:18:35 +1100

    add mbyte/gbyte constants

--- src/neurosis/constants.py
+++ src/neurosis/constants.py
@@ -7,3 +7,7 @@ IMAGE_EXTNS = [".jpg", ".jpeg", ".png", ".gif", ".webp", ".bmp", ".tiff", ".tif"
 CHECKPOINT_EXTNS = [".pt", ".pth", ".ckpt", ".safetensors"]
 
 TEST_DS_PATH = PACKAGE_ROOT.parent.parent.joinpath("data/ine")
+
+# misc size stuff
+MBYTE = 2**20
+GBYTE = 2**30
