b4c5fe5defef685686f590110bc125f4bd35fb32
Andrew Powers-Holmes <aholmes@omnom.net>
2023-12-06 23:13:24 +1100

    add batch hooks to hook

--- src/neurosis/modules/losses/hooks.py
+++ src/neurosis/modules/losses/hooks.py
@@ -19,3 +19,22 @@ class LossHook(ABC):
         loss_dict: dict[str, Tensor] = {},
     ) -> tuple[Tensor, dict[str, Tensor]]:
         raise NotImplementedError("You called an ABC. Why would you do that? ;_;")
+
+    def on_train_batch_start(
+        self,
+        trainer: L.Trainer,
+        pl_module: L.LightningModule,
+        batch,
+        batch_idx,
+    ):
+        pass
+
+    def on_train_batch_end(
+        self,
+        trainer: L.Trainer,
+        pl_module: L.LightningModule,
+        outputs: Tensor,
+        batch: dict,
+        batch_idx: int,
+    ):
+        pass
