98476bf5390a88a5934a290b0a9103f37d2ef0a0
Andrew Powers-Holmes <aholmes@omnom.net>
2023-12-07 12:32:46 +1100

    harden np_text_decode against things that are strings already

--- src/neurosis/utils/text.py
+++ src/neurosis/utils/text.py
@@ -4,5 +4,5 @@ import numpy as np
 def np_text_decode(text: np.bytes_ | str | list):
     if not isinstance(text, list):
         text = [text]
-    text = [x.decode("utf-8") for x in text if isinstance(x, np.bytes_)]
+    text = [x.decode("utf-8") if isinstance(x, np.bytes_) else x for x in text]
     return text[0] if len(text) == 1 else text
