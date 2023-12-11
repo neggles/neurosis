9f63debba1de13093c5c36ef8f6dbed0865c4521
Andrew Powers-Holmes <aholmes@omnom.net>
2023-12-12 01:17:39 +1100

    add cosine

--- src/neurosis/schedulers/__init__.py
+++ src/neurosis/schedulers/__init__.py
@@ -1,10 +1,13 @@
 from .base import AbstractLRSchedule, BaseLRScheduler
+from .cosine import CosineAnnealingWarmupRestarts, LegacyCosineAnnealingWarmupRestarts
 from .warmup import CosineWarmUpSchedule, CosineWarmUpStagedSchedule, LinearWarmUpSchedule
 
 __all__ = [
     "AbstractLRSchedule",
     "BaseLRScheduler",
+    "CosineAnnealingWarmupRestarts",
     "CosineWarmUpSchedule",
     "CosineWarmUpStagedSchedule",
+    "LegacyCosineAnnealingWarmupRestarts",
     "LinearWarmUpSchedule",
 ]
