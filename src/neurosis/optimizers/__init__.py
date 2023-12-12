187400c9b7604fdacfb752aa6f3ef8c558ec7ad0
Andrew Powers-Holmes <aholmes@omnom.net>
2023-12-12 16:34:17 +1100

    add hybrid sched

--- src/neurosis/optimizers/__init__.py
+++ src/neurosis/optimizers/__init__.py
@@ -1,6 +1,9 @@
 from .adafactor import Adafactor, AdafactorScheduler
+from .hybrid import HybridOptimizer, HybridScheduler
 
 __all__ = [
     "Adafactor",
     "AdafactorScheduler",
+    "HybridOptimizer",
+    "HybridScheduler",
 ]
