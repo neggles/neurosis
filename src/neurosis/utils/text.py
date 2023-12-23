03727faeead17d2a23a5244f0b4763ee86dc89c8
Andrew Powers-Holmes <aholmes@omnom.net>
2023-12-23 20:32:33 +1100

    allow np_text_decode to always return a list

--- src/neurosis/utils/text.py
+++ src/neurosis/utils/text.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 
-def np_text_decode(text: np.bytes_ | str | list):
+def np_text_decode(text: np.bytes_ | str | list, aslist: bool = False):
     if not isinstance(text, list):
         text = [text]
     text = [x.decode("utf-8") if isinstance(x, np.bytes_) else x for x in text]
-    return text[0] if len(text) == 1 else text
+    if len(text) == 1 and not aslist:
+        text = text[0]
+    return text
