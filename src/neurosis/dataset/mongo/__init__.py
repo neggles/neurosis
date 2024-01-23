4a8a6df1b3958d28203ac70f4211ace7d95b18e8
Andrew Powers-Holmes <aholmes@omnom.net>
2024-01-23 22:01:38 +1100

    add some missing exports

--- src/neurosis/dataset/mongo/__init__.py
+++ src/neurosis/dataset/mongo/__init__.py
@@ -1,9 +1,15 @@
 from .dataset import MongoAspectDataset, MongoDbModule
+from .nobucket import MongoSquareDataset, MongoSquareModule
+from .nocaption import MongoVAEDataset, MongoVAEModule
 from .settings import MongoSettings, get_mongo_settings
 
 __all__ = [
     "MongoAspectDataset",
     "MongoDbModule",
     "MongoSettings",
+    "MongoSquareDataset",
+    "MongoSquareModule",
+    "MongoVAEDataset",
+    "MongoVAEModule",
     "get_mongo_settings",
 ]
