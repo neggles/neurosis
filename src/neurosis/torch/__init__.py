668e455a647257ef98fa1ac4b91636c67d920474
Andi Powers-Holmes <aholmes@omnom.net>
2024-01-24 05:40:36 +0000

    add an hook

--- src/neurosis/torch/__init__.py
+++ src/neurosis/torch/__init__.py
@@ -1,6 +1,8 @@
+from .hooks import FreezeSliceHook
 from .stringtensor import StringArray, StringEncoding, StringTensor
 
 __all__ = [
+    "FreezeSliceHook",
     "StringArray",
     "StringEncoding",
     "StringTensor",
