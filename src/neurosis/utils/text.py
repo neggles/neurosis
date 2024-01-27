9c4f04b10c4a4f952e335b2ee75e8211d1310b08
Andi Powers-Holmes <aholmes@omnom.net>
2024-01-27 08:02:50 +0000

    text func touchup

--- src/neurosis/utils/text.py
+++ src/neurosis/utils/text.py
@@ -4,7 +4,7 @@ import numpy as np
 def np_text_decode(text: np.bytes_ | str | list, aslist: bool = False):
     if not isinstance(text, list):
         text = [text]
-    text = [x.decode("utf-8") if isinstance(x, np.bytes_) else x for x in text]
+    text = [x.decode("utf-8") if isinstance(x, (np.bytes_, bytes)) else x for x in text]
     if len(text) == 1 and not aslist:
         text = text[0]
     return text
