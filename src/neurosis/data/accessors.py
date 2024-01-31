fd18216961c65a490a2a6c0d6a627fb6c00e5438
Andi Powers-Holmes <aholmes@omnom.net>
2024-01-31 01:04:29 +0000

    refactor lpips accessor

--- src/neurosis/data/accessors.py
+++ src/neurosis/data/accessors.py
@@ -5,6 +5,8 @@ from io import BufferedReader
 from typing import Any, Generator
 
 from PIL import ImageFont
+from safetensors.torch import load_file as safetensors_load
+from torch import load as torch_load
 
 from .files import open_package_file
 
@@ -17,11 +19,12 @@ def get_image_font(name: str = "NotoSansMono", size: int = 10) -> ImageFont.Free
 
 
 @contextmanager
-def lpips_checkpoint(name: str = "vgg_lpips") -> Generator[BufferedReader, Any, None]:
-    lpips_file = resources.files(f"{__package__}.lpips").joinpath(f"{name}.pth")
-    if not lpips_file.exists():
-        raise FileNotFoundError(f"File {lpips_file} not found in {__package__}.lpips")
+def lpips_checkpoint(name: str = "vgg_lpips_v0.1") -> Generator[BufferedReader, Any, None]:
+    rsrc_dir = resources.files(f"{__package__}.lpips")
     try:
-        yield lpips_file.open("rb")
+        if rsrc_dir.joinpath(f"{name}.safetensors").exists():
+            yield safetensors_load(rsrc_dir.joinpath(f"{name}.safetensors"))
+        elif rsrc_dir.joinpath(f"{name}.pth").exists():
+            yield torch_load(rsrc_dir.joinpath(f"{name}.pth"), map_location="cpu", weights_only=True)
     finally:
         pass
