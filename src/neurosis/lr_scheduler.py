ff38fd517f9538ea752a31d2d8ab72bbb64c7d82
Andi Powers-Holmes <aholmes@omnom.net>
2024-02-03 23:33:45 +0000

    goodbye `black` hello `ruff format`

--- src/neurosis/lr_scheduler.py
+++ src/neurosis/lr_scheduler.py
@@ -131,8 +131,11 @@ class LambdaLinearScheduler(LambdaWarmUpCosineScheduler2):
             self.last_f = f
             return f
         else:
-            f = self.f_min[cycle] + (self.f_max[cycle] - self.f_min[cycle]) * (
-                self.cycle_lengths[cycle] - n
-            ) / (self.cycle_lengths[cycle])
+            f = (
+                self.f_min[cycle]
+                + (self.f_max[cycle] - self.f_min[cycle])
+                * (self.cycle_lengths[cycle] - n)
+                / (self.cycle_lengths[cycle])
+            )
             self.last_f = f
             return f
