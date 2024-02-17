aa73d4a3ae98c3355e134d76f84eb48e9104af2d
Andrew Powers-Holmes <aholmes@omnom.net>
2024-02-17 11:27:27 +1100

    add image label stamping helper

--- src/neurosis/utils/image/__init__.py
+++ src/neurosis/utils/image/__init__.py
@@ -1,9 +1,14 @@
 from .convert import numpy_to_pil, numpy_to_pt, pil_to_numpy, pil_to_pt, pt_to_numpy, pt_to_pil
+from .grid import CaptionGrid
+from .label import label_batch, label_image
 from .pil import pil_ensure_rgb, pil_pad_square
 from .vae import denormalize, normalize
 
 __all__ = [
+    "CaptionGrid",
     "denormalize",
+    "label_batch",
+    "label_image",
     "normalize",
     "numpy_to_pil",
     "numpy_to_pt",
