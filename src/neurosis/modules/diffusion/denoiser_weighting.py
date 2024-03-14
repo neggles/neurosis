1ef7327d13c4ca43932f89ab64ddcf0437e9814c
Andi Powers-Holmes <aholmes@omnom.net>
2024-03-14 09:26:54 +0000

    minor refactor

--- src/neurosis/modules/diffusion/denoiser_weighting.py
+++ src/neurosis/modules/diffusion/denoiser_weighting.py
@@ -5,28 +5,35 @@ from torch import Tensor
 
 
 class DenoiserWeighting(ABC):
+    def __init__(self):
+        super().__init__()
+
     def __call__(self, sigma: Tensor) -> Tensor:
         raise NotImplementedError("Abstract base class was called ;_;")
 
 
 class UnitWeighting(DenoiserWeighting):
     def __call__(self, sigma: Tensor) -> Tensor:
-        return torch.ones_like(sigma, device=sigma.device)
+        weights = torch.ones_like(sigma, device=sigma.device)
+        return weights
+
+
+class EpsWeighting(DenoiserWeighting):
+    def __call__(self, sigma: Tensor) -> Tensor:
+        weights = sigma**-2.0
+        return weights
 
 
 class EDMWeighting(DenoiserWeighting):
     def __init__(self, sigma_data: float = 0.5):
+        super().__init__()
         self.sigma_data = sigma_data
 
     def __call__(self, sigma: Tensor) -> Tensor:
-        return (sigma**2 + self.sigma_data**2) / (sigma * self.sigma_data) ** 2
+        weights = (sigma**2 + self.sigma_data**2) / (sigma * self.sigma_data) ** 2
+        return weights
 
 
 class VWeighting(EDMWeighting):
     def __init__(self):
         super().__init__(sigma_data=1.0)
-
-
-class EpsWeighting(DenoiserWeighting):
-    def __call__(self, sigma):
-        return sigma**-2.0
