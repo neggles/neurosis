6fcd822f17b31f372ae93628e1be9d394f754dae
Andi Powers-Holmes <aholmes@omnom.net>
2024-03-14 09:28:49 +0000

    make sure all the values being used in denoiser_scaling are floats

--- src/neurosis/modules/diffusion/denoiser_scaling.py
+++ src/neurosis/modules/diffusion/denoiser_scaling.py
@@ -16,9 +16,9 @@ class EDMScaling(DenoiserScaling):
         self.sigma_data = sigma_data
 
     def __call__(self, sigma: Tensor) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
-        c_skip = self.sigma_data**2 / (sigma**2 + self.sigma_data**2)
-        c_out = sigma * self.sigma_data / (sigma**2 + self.sigma_data**2) ** 0.5
-        c_in = 1 / (sigma**2 + self.sigma_data**2) ** 0.5
+        c_skip = self.sigma_data**2.0 / (sigma**2.0 + self.sigma_data**2.0)
+        c_out = sigma * self.sigma_data / (sigma**2.0 + self.sigma_data**2.0) ** 0.5
+        c_in = 1.0 / (sigma**2.0 + self.sigma_data**2.0) ** 0.5
         c_noise = 0.25 * sigma.log()
         return c_skip, c_out, c_in, c_noise
 
@@ -27,24 +27,24 @@ class EpsScaling(DenoiserScaling):
     def __call__(self, sigma: Tensor) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
         c_skip = torch.ones_like(sigma, device=sigma.device)
         c_out = -sigma
-        c_in = 1 / (sigma**2 + 1.0) ** 0.5
+        c_in = 1.0 / (sigma**2.0 + 1.0) ** 0.5
         c_noise = sigma.clone()
         return c_skip, c_out, c_in, c_noise
 
 
 class VScaling(DenoiserScaling):
     def __call__(self, sigma: Tensor) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
-        c_skip = 1.0 / (sigma**2 + 1.0)
-        c_out = -sigma / (sigma**2 + 1.0) ** 0.5
-        c_in = 1.0 / (sigma**2 + 1.0) ** 0.5
+        c_skip = 1.0 / (sigma**2.0 + 1.0)
+        c_out = -sigma / (sigma**2.0 + 1.0) ** 0.5
+        c_in = 1.0 / (sigma**2.0 + 1.0) ** 0.5
         c_noise = sigma.clone()
         return c_skip, c_out, c_in, c_noise
 
 
 class VScalingWithEDMcNoise(DenoiserScaling):
     def __call__(self, sigma: Tensor) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
-        c_skip = 1.0 / (sigma**2 + 1.0)
-        c_out = -sigma / (sigma**2 + 1.0) ** 0.5
-        c_in = 1.0 / (sigma**2 + 1.0) ** 0.5
+        c_skip = 1.0 / (sigma**2.0 + 1.0)
+        c_out = -sigma / (sigma**2.0 + 1.0) ** 0.5
+        c_in = 1.0 / (sigma**2.0 + 1.0) ** 0.5
         c_noise = 0.25 * sigma.log()
         return c_skip, c_out, c_in, c_noise
