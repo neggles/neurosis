28a19aa14e9e2f39f965b700eb4cd5a9ced2e821
Andi Powers-Holmes <aholmes@omnom.net>
2024-03-17 01:46:12 +0000

    add pandas COW mode

--- src/neurosis/__init__.py
+++ src/neurosis/__init__.py
@@ -11,6 +11,7 @@ from functools import lru_cache
 from os import getenv
 from pathlib import Path
 
+import pandas as pd
 from einops._torch_specific import allow_ops_in_compiled_graph
 from PIL import Image
 from rich.console import Console
@@ -29,14 +30,19 @@ if is_debug is True:
 
 _ = _install_traceback(show_locals=is_debug, width=120, word_wrap=True)
 
-allow_ops_in_compiled_graph()
-del allow_ops_in_compiled_graph
-
+# set up the console
 console = Console(highlight=not is_debug)
 
 if is_debug:
     console.log("[bold red]NEUROSIS_DEBUG[/bold red] is set")
     console.log("Enabling use of einops functions in TorchDynamo graphs...")
+
+# enable einops to be used in torch.compile()
+allow_ops_in_compiled_graph()
+del allow_ops_in_compiled_graph
+
+# enable Pandas copy-on-write
+pd.options.mode.copy_on_write = True
 
 
 @lru_cache(maxsize=4)
