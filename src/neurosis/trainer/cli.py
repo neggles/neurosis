dab78458ed94b18be9b8ab6707f0bfecd10aec09
Andi Powers-Holmes <aholmes@omnom.net>
2024-03-21 02:19:09 +0000

    rework cli a bit to make it ensure the wandb dir exists where relevant

--- src/neurosis/trainer/cli.py
+++ src/neurosis/trainer/cli.py
@@ -1,18 +1,14 @@
 import logging
 from os import getenv, isatty
 from pathlib import Path
-from typing import Annotated, List, Optional
+from typing import Annotated, Optional
 from warnings import filterwarnings
 
 import jsonargparse
 import lightning
 import torch
 import typer
-from lightning.pytorch.callbacks import (  # noqa: F401
-    LearningRateMonitor,
-    ModelCheckpoint,
-    ModelSummary,
-)
+from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.cli import ArgsType, LightningArgumentParser, LightningCLI
 from rich.traceback import install as install_traceback
 
@@ -26,8 +22,9 @@ if isatty(1) and getenv("KUBERNETES_PORT", None) is not None:
         console=console,
         suppress=[jsonargparse, torch, lightning],
         show_locals=is_debug,
-        locals_max_length=3,
+        locals_max_length=1,
         locals_max_string=64,
+        max_frames=20,
     )
 del install_traceback
 
@@ -53,6 +50,33 @@ class DiffusionTrainerCli(LightningCLI):
             required=False,
         )
 
+    def before_instantiate_classes(self) -> None:
+        if self.config.subcommand != "fit":
+            logger.debug(f"Skipping before_instantiate_classes hook for {self.config.subcommand}")
+            return
+        logger.debug("Running before_instantiate_classes hook")
+        if trainer_args := self.config.get("fit", {}).get("trainer"):
+            if hasattr(trainer_args, "default_root_dir"):
+                default_root_dir = Path(trainer_args.default_root_dir).resolve()
+                logger.info(f"Making sure trainer workdir {default_root_dir} exists")
+                default_root_dir.mkdir(exist_ok=True, parents=True)
+
+            if train_loggers := trainer_args.get("logger"):
+                logger.info(f"Got loggers: {train_loggers}")
+                for lobj in [
+                    x
+                    for x in train_loggers
+                    if (
+                        hasattr(x, "class_path")
+                        and hasattr(x, "init_args")
+                        and "wandb" in x.class_path.lower()
+                    )
+                ]:
+                    if save_dir := lobj.init_args.get("save_dir"):
+                        save_dir = Path(save_dir).resolve()
+                        logger.info(f"Making sure wandb save_dir {save_dir} exists")
+                        save_dir.mkdir(exist_ok=True, parents=True)
+
 
 @train_app.command(add_help_option=False)
 def main(
@@ -67,25 +91,24 @@ def main(
     logger.info(f"neurosis v{__version__} training script running!")
     logger.debug(f"PyTorch {torch.__version__}, Lightning {lightning.__version__}")
 
-    default_config_files: List[str] = []
-    if Path.cwd().joinpath("configs/lightning/defaults.yaml").exists():
-        default_config_files.append("configs/lightning/defaults.yaml")
-
-    if hasattr(torch.backends, "cudnn") and torch.backends.cudnn.is_available():
-        logger.info("Enabling TensorFloat32 in cuDNN library")
-        torch.backends.cudnn.allow_tf32 = True
-
-    if hasattr(torch, "cuda") and torch.cuda.is_available():
-        logger.info("Enabling TensorFloat32 in CUDA library")
-        torch.backends.cuda.matmul.allow_tf32 = True
-
     # temporary since Diffusers triggers this warning on Torch 2.2.1 atm
+    logger.debug("Filtering out torch.utils._pytree._register_pytree_node warning (caused by Diffusers)")
     filterwarnings("ignore", category=UserWarning, message=r"torch\.utils\._pytree\._register_pytree_node.*")
 
-    torch.set_float32_matmul_precision("high")  # enables tf32 stuff
+    allow_tf32 = getenv("NEUROSIS_DISABLE_TF32", "").lower() not in ["1", "true", "yes"]
+    if allow_tf32:
+        logger.info("Enabling matmul performance optimizations (tf32 and 'high' precision)")
+        torch.set_float32_matmul_precision("high")
+    else:
+        logger.warning("Disabling matmul optimizations as NEUROSIS_DISABLE_TF32 is set")
+        torch.set_float32_matmul_precision("highest")
 
-    if hasattr(args, "trainer") and hasattr(args.trainer, "default_root_dir"):
-        Path(args.trainer.default_root_dir).mkdir(exist_ok=True, parents=True)
+    if hasattr(torch, "cuda") and torch.cuda.is_available():
+        torch.backends.cuda.matmul.allow_tf32 = allow_tf32
+        logger.info(f"{'Enabled' if allow_tf32 else 'Disabled'} tf32 in CUDA")
+    if hasattr(torch.backends, "cudnn") and torch.backends.cudnn.is_available():
+        torch.backends.cudnn.allow_tf32 = allow_tf32
+        logger.info(f"{'Enabled' if allow_tf32 else 'Disabled'} tf32 in cuDNN")
 
     cli = DiffusionTrainerCli(  # type: ignore
         datamodule_class=None,
@@ -98,7 +121,6 @@ def main(
         ),
         parser_kwargs=dict(
             default_env=True,
-            default_config_files=default_config_files,
             parser_mode="omegaconf",
         ),
         save_config_callback=LoggerSaveConfigCallback,
