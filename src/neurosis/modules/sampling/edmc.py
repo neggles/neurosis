38deb7ca1152c93d15da5f97da49939e4e0e3291
Andi Powers-Holmes <aholmes@omnom.net>
2024-03-28 04:21:46 +0000

    add tan schedule sampler to new samplers

--- src/neurosis/modules/sampling/edmc.py
+++ src/neurosis/modules/sampling/edmc.py
@@ -35,3 +35,21 @@ class ContinuousEDMSampler(DiffusionSampler2):
 
         log_sigma_min = math.log(self.sigma_min)
         return math.exp((math.log(self.sigma_max) - log_sigma_min) * percent + log_sigma_min)
+
+
+class TanEDMSampler(ContinuousEDMSampler):
+    def __init__(
+        self,
+        sigma_min: float = 0.001,
+        sigma_max: float = 1000.0,
+        sigma_data: float = 1.0,
+        eps: float = 5e-3,
+    ):
+        super().__init__()
+        self._sigma_min = sigma_min
+        self._sigma_max = sigma_max
+        self._eps = eps
+
+        half_pi_t = torch.acos(torch.zeros(1, dtype=torch.float64)).squeeze(0)
+        sigmas = torch.tan(torch.linspace(0.0, half_pi_t - eps, 1000, dtype=torch.float64)).to(torch.float32)
+        self.set_sigmas(sigmas, sigma_data)
