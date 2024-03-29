3cad087ad1c24ed1c8924729a8687ea3356c81db
Andi Powers-Holmes <aholmes@omnom.net>
2024-03-29 11:06:21 +0000

    rework FSDP policies a bit, add lowmem one

--- src/neurosis/utils/fsdp_custom_policy.py
+++ src/neurosis/utils/fsdp_custom_policy.py
@@ -1,5 +1,7 @@
+from clip.model import ResidualAttentionBlock
 from torch import nn
 from torch.distributed.fsdp.wrap import transformer_auto_wrap_policy
+from transformers.models.clip.modeling_clip import CLIPEncoderLayer
 
 from neurosis.models.embedding import GeneralConditioner
 from neurosis.models.text_encoder import (
@@ -18,7 +20,12 @@ from neurosis.modules.diffusion.model import (
     ResnetBlock,
     TorchSDPAttnBlock,
 )
-from neurosis.modules.diffusion.openaimodel import ResBlock, SpatialTransformer, UNetModel
+from neurosis.modules.diffusion.openaimodel import (
+    ResBlock,
+    SpatialTransformer,
+    TimestepEmbedSequential,
+    UNetModel,
+)
 
 
 def diffusion_fsdp_policy(
@@ -32,7 +39,35 @@ def diffusion_fsdp_policy(
         nonwrapped_numel,
         transformer_layer_cls={
             # AutoencodingEngine,
+            CLIPEncoderLayer,
+            Decoder,
+            Encoder,
+            FrozenCLIPEmbedder,
+            FrozenOpenCLIPEmbedder,
+            FrozenOpenCLIPEmbedder2,
+            FrozenT5Embedder,
+            GeneralConditioner,
+            ResidualAttentionBlock,
+            SpatialTransformer,
+            TimestepEmbedSequential,
+            UNetModel,
+        },
+    )
+
+
+def diffusion_fsdp_lowmem_policy(
+    module: nn.Module,
+    recurse: bool,
+    nonwrapped_numel: int,
+) -> bool:
+    return transformer_auto_wrap_policy(
+        module,
+        recurse,
+        nonwrapped_numel,
+        transformer_layer_cls={
+            # AutoencodingEngine,
             AttnBlock,
+            CLIPEncoderLayer,
             Decoder,
             Encoder,
             FrozenCLIPEmbedder,
@@ -44,8 +79,10 @@ def diffusion_fsdp_policy(
             MemoryEfficientAttnBlock,
             MemoryEfficientCrossAttentionWrapper,
             ResBlock,
+            ResidualAttentionBlock,
             ResnetBlock,
             SpatialTransformer,
+            TimestepEmbedSequential,
             TorchSDPAttnBlock,
             UNetModel,
         },
