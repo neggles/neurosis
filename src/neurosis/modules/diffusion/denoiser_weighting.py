0b03ba07415d6d7a9a49b59a6e739106fa77dc42
Andi Powers-Holmes <aholmes@omnom.net>
2024-03-30 03:12:02 +0000

    remove commented-out code

--- src/neurosis/modules/diffusion/denoiser_weighting.py
+++ src/neurosis/modules/diffusion/denoiser_weighting.py
@@ -32,26 +32,3 @@ class EDMWeighting(DenoiserWeighting):
     def __call__(self, sigma: Tensor) -> Tensor:
         weights = (sigma**2 + self.sigma_data**2) / (sigma * self.sigma_data) ** 2
         return weights
-
-
-# class VWeighting(EDMWeighting):
-#     def __init__(self):
-#         super().__init__(sigma_data=1.0)
-
-
-# class MinSNRGammaVWeighting(VWeighting):
-#     def __init__(self, gamma: float = 5.0):
-#         super().__init__()
-#         self.gamma = gamma
-
-#     def __call__(self, sigma: Tensor) -> Tensor:
-#         # get regular v-pred weights
-#         v_weights = super().__call__(sigma)
-#         # calculate SNR, assuming unit variance
-#         snr = 1 / sigma**2
-#         # get min(snr, gamma) for each element
-#         min_snr_gamma = torch.min(snr, torch.full_like(snr, self.gamma))
-#         # prevent div/0 errors with v-prediction
-#         snr_weight = min_snr_gamma.div(snr + 1)
-#         # return the adjusted weights
-#         return v_weights * snr_weight
