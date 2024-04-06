67b962128cdf970cd837ead9809ecf0896b64a62
Andi Powers-Holmes <aholmes@omnom.net>
2024-04-06 11:04:36 +0000

    add min-snr-gamma modifier weight module

--- src/neurosis/modules/diffusion/denoiser_weighting.py
+++ src/neurosis/modules/diffusion/denoiser_weighting.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 import torch
 from torch import Tensor
@@ -8,6 +8,7 @@ class DenoiserWeighting(ABC):
     def __init__(self):
         super().__init__()
 
+    @abstractmethod
     def __call__(self, sigma: Tensor) -> Tensor:
         raise NotImplementedError("Abstract base class was called ;_;")
 
@@ -52,3 +53,29 @@ class RectifiedFlowWeighting(DenoiserWeighting):
             * torch.exp(-0.5 * (torch.log(sigma) - self.m) ** 2 / self.s**2)
         )
         return cfm_weights * pi_weights
+
+
+class MinSNRGammaModifier(DenoiserWeighting):
+    def __init__(
+        self,
+        weighting: DenoiserWeighting,
+        gamma: float = 5,
+        v_pred: bool = False,
+    ):
+        super().__init__()
+        self.weighting = weighting
+        self.gamma = gamma
+        self.v_pred = v_pred
+
+    def __call__(self, sigma: Tensor) -> Tensor:
+        weights = self.weighting(sigma)
+        snr = 1.0 / sigma**2
+
+        # get min(snr, gamma) for each element
+        snr_weight = torch.min(snr, torch.full_like(snr, self.gamma))
+        if self.v_pred:
+            snr_weight = snr_weight.div(snr + 1.0)
+        else:
+            snr_weight = snr_weight.div(snr)
+
+        return weights * snr_weight
