3645345c72b07284de200dbf8331ab0f4e71972a
Andi Powers-Holmes <aholmes@omnom.net>
2024-04-07 02:47:24 +0000

    rework as-yet-unused common LRSchedule components

--- src/neurosis/schedulers/base.py
+++ src/neurosis/schedulers/base.py
@@ -1,6 +1,8 @@
 import logging
 from abc import ABC, abstractmethod
+from warnings import warn
 
+from torch import Tensor
 from torch.optim.lr_scheduler import LambdaLR
 from torch.optim.optimizer import Optimizer
 
@@ -10,15 +12,23 @@ logger = logging.getLogger(__name__)
 class AbstractLRSchedule(ABC):
     last_epoch: int
 
-    @abstractmethod
-    def schedule(self, n: int, **kwargs) -> float:
-        raise NotImplementedError("Abstract base class was called ;_;")
+    def __init__(self, *args, **kwargs):
+        if len(args) > 0:
+            logger.warning(f"{self.__class__.__name__}: ignored extra args: {args}")
+        if len(kwargs) > 0:
+            logger.warning(f"{self.__class__.__name__}: ignored extra kwargs: {kwargs}")
 
-    def __call__(self, n: int, **kwargs):
-        return self.schedule(n, **kwargs)
+    def __call__(self, step: int, **kwargs):
+        return self.schedule(step, **kwargs)
+
+    @abstractmethod
+    def schedule(self, step: int, **kwargs) -> float | Tensor:
+        raise NotImplementedError("Abstract base class was called ;_;")
 
 
 class BaseLRScheduler(LambdaLR):
+    _get_lr_called_within_step: bool
+
     def __init__(
         self,
         optimizer: Optimizer,
@@ -31,14 +41,16 @@ class BaseLRScheduler(LambdaLR):
 
         if not isinstance(schedule, (list, tuple)):
             schedules = [schedule] * num_groups
-        elif len(schedules) != num_groups:
+
+        if len(schedules) != num_groups:
             raise ValueError(f"expected 1 or {num_groups} schedules, got {len(schedules)}")
         else:
             logger.info(f"using {len(schedule)} schedules for {num_groups} param groups")
             schedules = list(schedule)
+
         super().__init__(optimizer, schedules, last_epoch, verbose)
 
-    def __call__(self, n: int, **kwargs):
-        if n is None:
-            n = self.last_epoch
-        return self.schedule(n, **kwargs)
+    def get_lr(self):
+        if not self._get_lr_called_within_step:
+            warn("To get the last learning rate computed by the scheduler, please use `get_last_lr()`.")
+        return [sched(self.last_epoch) for sched in self.lr_lambdas]
