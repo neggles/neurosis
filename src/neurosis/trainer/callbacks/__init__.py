7bad43634adb47de7ce2e1167101b79ae13a3681
Andi Powers-Holmes <aholmes@omnom.net>
2024-04-07 05:22:35 +0000

    add ConflictAbortCallback (untested)

--- src/neurosis/trainer/callbacks/__init__.py
+++ src/neurosis/trainer/callbacks/__init__.py
@@ -3,19 +3,21 @@ from .exception import ExceptionHandlerCallback
 from .image_logger import ImageLogger
 from .progress import NeurosisProgressBar, NeurosisProgressTheme
 from .refimg_logger import ReferenceModelImageLogger
-from .setup import SetupCallback, TF32Callback
+from .setup import TF32Callback
 from .stats import GPUMemoryUsage
+from .system import ConflictAbortCallback
 from .wandb import WandbLogger
 
 __all__ = [
-    "HFHubCheckpoint",
+    "ConflictAbortCallback",
     "ExceptionHandlerCallback",
+    "GPUMemoryUsage",
+    "HFHubCheckpoint",
     "ImageLogger",
     "NeurosisProgressBar",
     "NeurosisProgressTheme",
     "ReferenceModelImageLogger",
     "SetupCallback",
     "TF32Callback",
-    "GPUMemoryUsage",
     "WandbLogger",
 ]
