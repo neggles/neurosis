35a3692abed3ebd7d32921ce4c3554dd48184fb3
Andi Powers-Holmes <aholmes@omnom.net>
2024-04-07 06:53:42 +0000

    kill old ray dataloader

--- src/neurosis/dataset/utils.py
+++ src/neurosis/dataset/utils.py
@@ -1,5 +1,6 @@
 from os import PathLike
 from pathlib import Path
+from typing import TypeAlias
 
 import numpy as np
 import torch
@@ -8,6 +9,8 @@ from torch import Tensor, nn
 
 from neurosis.dataset.aspect.bucket import AspectBucket
 from neurosis.utils.image import pil_ensure_rgb
+
+SampleType: TypeAlias = dict[str, Tensor | list[Tensor | np.ndarray]]
 
 
 class VAENormalize(nn.Module):
