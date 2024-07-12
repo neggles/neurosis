2be9e5c5916da7722038a2551cfe43ec95eea285
Andi Powers-Holmes <aholmes@omnom.net>
2024-07-12 05:28:31 +0000

    export ConcatTimestepEmbedderND from modules.encoders

--- src/neurosis/modules/encoders/__init__.py
+++ src/neurosis/modules/encoders/__init__.py
@@ -8,12 +8,14 @@ from .embedding import (
     SpatialRescaler,
 )
 from .lowscale import LowScaleEncoder
+from .metadata import ConcatTimestepEmbedderND
 from .misc import IdentityEncoder
 
 __all__ = [
     "AbstractEmbModel",
     "ClassEmbedder",
     "ClassEmbedderForMultiCond",
+    "ConcatTimestepEmbedderND",
     "GeneralConditioner",
     "IdentityEncoder",
     "LowScaleEncoder",
